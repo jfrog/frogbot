2c3ad92404afe688ca515b1c700ba5b4a9071c86
Tal Arian <tala@jfrog.com>
2022-03-29 19:04:30 +0300

    Expand Frogbot tests

--- commands/scanpullrequest_test.go
+++ commands/scanpullrequest_test.go
@@ -2,12 +2,15 @@ package commands
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/jfrog/frogbot/commands/testdata"
 	"github.com/jfrog/frogbot/commands/utils"
 	"github.com/jfrog/froggit-go/vcsclient"
+	xrayutils "github.com/jfrog/jfrog-cli-core/v2/xray/utils"
 	"github.com/jfrog/jfrog-client-go/xray/services"
 	"github.com/stretchr/testify/assert"
 )
@@ -82,6 +85,74 @@ func TestGetNewViolations(t *testing.T) {
 	assert.ElementsMatch(t, []string{"component-C", "component-D"}, []string{impactedPackageOne, impactedPackageTwo})
 }
 
+func TestGetNewViolationsCaseNoPrevViolations(t *testing.T) {
+	// Previous scan with no violation
+	previousScan := services.ScanResponse{
+		Violations: []services.Violation{},
+	}
+
+	// Current scan with 2 violations - XRAY-1 and XRAY-2
+	currentScan := services.ScanResponse{
+		Violations: []services.Violation{
+			{
+				IssueId:       "XRAY-1",
+				Summary:       "summary-1",
+				Severity:      "high",
+				ViolationType: "security",
+				Components:    map[string]services.Component{"component-A": {}},
+			},
+			{
+				IssueId:       "XRAY-2",
+				Summary:       "summary-2",
+				ViolationType: "security",
+				Severity:      "low",
+				Components:    map[string]services.Component{"component-C": {}},
+			},
+		},
+	}
+
+	// Run getNewViolations and expect both XRAY-1 and XRAY-2 violation in the results
+	newViolations := getNewViolations(previousScan, currentScan)
+	assert.Len(t, newViolations, 2)
+	assert.Equal(t, "XRAY-1", newViolations[0].IssueId)
+	assert.Equal(t, "high", newViolations[0].Severity)
+	assert.Equal(t, "component-A", newViolations[0].ImpactedPackageName)
+	assert.Equal(t, "XRAY-2", newViolations[1].IssueId)
+	assert.Equal(t, "low", newViolations[1].Severity)
+	assert.Equal(t, "component-C", newViolations[1].ImpactedPackageName)
+}
+
+func TestGetNewViolationsCaseNoNewViolations(t *testing.T) {
+	// Previous scan with 2 violations - XRAY-1 and XRAY-2
+	previousScan := services.ScanResponse{
+		Violations: []services.Violation{
+			{
+				IssueId:       "XRAY-1",
+				Summary:       "summary-1",
+				Severity:      "high",
+				ViolationType: "security",
+				Components:    map[string]services.Component{"component-A": {}},
+			},
+			{
+				IssueId:       "XRAY-2",
+				Summary:       "summary-2",
+				ViolationType: "security",
+				Severity:      "low",
+				Components:    map[string]services.Component{"component-C": {}},
+			},
+		},
+	}
+
+	// Current scan with no violation
+	currentScan := services.ScanResponse{
+		Violations: []services.Violation{},
+	}
+
+	// Run getNewViolations and expect no violations in the results
+	newViolations := getNewViolations(previousScan, currentScan)
+	assert.Len(t, newViolations, 0)
+}
+
 func TestGetNewVulnerabilities(t *testing.T) {
 	// Previous scan with only one vulnerability - XRAY-1
 	previousScan := services.ScanResponse{
@@ -122,6 +193,70 @@ func TestGetNewVulnerabilities(t *testing.T) {
 	impactedPackageOne := newViolations[0].ImpactedPackageName
 	impactedPackageTwo := newViolations[1].ImpactedPackageName
 	assert.ElementsMatch(t, []string{"component-C", "component-D"}, []string{impactedPackageOne, impactedPackageTwo})
+}
+
+func TestGetNewVulnerabilitiesCaseNoPrevVulnerabilities(t *testing.T) {
+	// Previous scan with no vulnerabilities
+	previousScan := services.ScanResponse{
+		Vulnerabilities: []services.Vulnerability{},
+	}
+
+	// Current scan with 2 vulnerabilities - XRAY-1 and XRAY-2
+	currentScan := services.ScanResponse{
+		Vulnerabilities: []services.Vulnerability{
+			{
+				IssueId:    "XRAY-1",
+				Summary:    "summary-1",
+				Severity:   "high",
+				Components: map[string]services.Component{"component-A": {}},
+			},
+			{
+				IssueId:    "XRAY-2",
+				Summary:    "summary-2",
+				Severity:   "low",
+				Components: map[string]services.Component{"component-B": {}},
+			},
+		},
+	}
+
+	// Run getNewVulnerabilities and expect both XRAY-1 and XRAY-2 vulnerability in the results
+	newViolations := getNewVulnerabilities(previousScan, currentScan)
+	assert.Len(t, newViolations, 2)
+	assert.Equal(t, "XRAY-2", newViolations[0].IssueId)
+	assert.Equal(t, "low", newViolations[0].Severity)
+	assert.Equal(t, "component-A", newViolations[0].ImpactedPackageName)
+	assert.Equal(t, "XRAY-2", newViolations[1].IssueId)
+	assert.Equal(t, "low", newViolations[1].Severity)
+	assert.Equal(t, "component-B", newViolations[1].ImpactedPackageName)
+}
+
+func TestGetNewVulnerabilitiesCaseNoNewVulnerabilities(t *testing.T) {
+	// Previous scan with 2 vulnerabilities - XRAY-1 and XRAY-2
+	previousScan := services.ScanResponse{
+		Vulnerabilities: []services.Vulnerability{
+			{
+				IssueId:    "XRAY-1",
+				Summary:    "summary-1",
+				Severity:   "high",
+				Components: map[string]services.Component{"component-A": {}},
+			},
+			{
+				IssueId:    "XRAY-2",
+				Summary:    "summary-2",
+				Severity:   "low",
+				Components: map[string]services.Component{"component-B": {}},
+			},
+		},
+	}
+
+	// Current scan with no vulnerabilities
+	currentScan := services.ScanResponse{
+		Vulnerabilities: []services.Vulnerability{},
+	}
+
+	// Run getNewVulnerabilities and expect no vulnerability in the results
+	newViolations := getNewVulnerabilities(previousScan, currentScan)
+	assert.Len(t, newViolations, 0)
 }
 
 var params = &utils.FrogbotParams{
@@ -191,4 +326,76 @@ func mockVcsClient(t *testing.T) (*testdata.MockVcsClient, func()) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	return testdata.NewMockVcsClient(mockCtrl), mockCtrl.Finish
+}
+
+func TestCreatePullRequestMessageNoVulnerabilities(t *testing.T) {
+	vulnerabilities := []xrayutils.VulnerabilityRow{}
+	message := createPullRequestMessage(vulnerabilities)
+
+	expectedMessage, err := os.ReadFile(filepath.Join("testdata", "messages", "novulnerabilities.md"))
+	assert.NoError(t, err)
+	assert.Equal(t, string(expectedMessage), message)
+
+}
+
+func TestCreatePullRequestMessage(t *testing.T) {
+	vulnerabilities := []xrayutils.VulnerabilityRow{
+		{
+			Severity:               "High",
+			ImpactedPackageName:    "github.com/nats-io/nats-streaming-server",
+			ImpactedPackageVersion: "v0.21.0",
+			FixedVersions:          "[0.24.1]",
+			Components: []xrayutils.ComponentRow{
+				{
+					Name:    "github.com/nats-io/nats-streaming-server",
+					Version: "v0.21.0",
+				},
+			},
+			Cves: []xrayutils.CveRow{
+				{
+					Id: "CVE-2022-24450",
+				},
+			},
+		},
+		{
+			Severity:               "High",
+			ImpactedPackageName:    "github.com/mholt/archiver/v3",
+			ImpactedPackageVersion: "v3.5.1",
+			FixedVersions:          "",
+			Components: []xrayutils.ComponentRow{
+				{
+					Name:    "github.com/mholt/archiver/v3",
+					Version: "v3.5.1",
+				},
+			},
+			Cves: []xrayutils.CveRow{
+				{
+					Id: "",
+				},
+			},
+		},
+		{
+			Severity:               "Medium",
+			ImpactedPackageName:    "github.com/nats-io/nats-streaming-server",
+			ImpactedPackageVersion: "v0.21.0",
+			FixedVersions:          "[0.24.3]",
+			Components: []xrayutils.ComponentRow{
+				{
+					Name:    "github.com/nats-io/nats-streaming-server",
+					Version: "v0.21.0",
+				},
+			},
+			Cves: []xrayutils.CveRow{
+				{
+					Id: "CVE-2022-26652",
+				},
+			},
+		},
+	}
+	message := createPullRequestMessage(vulnerabilities)
+
+	expectedMessage, err := os.ReadFile(filepath.Join("testdata", "messages", "dummyvulnerabilities.md"))
+	assert.NoError(t, err)
+	assert.Equal(t, string(expectedMessage), message)
+
 }
---
+++ commands/testdata/messages/dummyvulnerabilities.md
@@ -0,0 +1,6 @@
+![](https://raw.githubusercontent.com/jfrog/frogbot/master/resources/vulnerabilitiesBanner.png)
+| SEVERITY | IMPACTED PACKAGE | IMPACTED PACKAGE  VERSION | FIXED VERSIONS | COMPONENT | COMPONENT VERSION | CVE
+:--: | -- | -- | -- | -- | :--: | --
+| ![](https://raw.githubusercontent.com/jfrog/frogbot/master/resources/highSeverity.png) High | github.com/nats-io/nats-streaming-server | v0.21.0 | [0.24.1] | github.com/nats-io/nats-streaming-server | v0.21.0 | CVE-2022-24450 
+| ![](https://raw.githubusercontent.com/jfrog/frogbot/master/resources/highSeverity.png) High | github.com/mholt/archiver/v3 | v3.5.1 |  | github.com/mholt/archiver/v3 | v3.5.1 |  
+| ![](https://raw.githubusercontent.com/jfrog/frogbot/master/resources/mediumSeverity.png) Medium | github.com/nats-io/nats-streaming-server | v0.21.0 | [0.24.3] | github.com/nats-io/nats-streaming-server | v0.21.0 | CVE-2022-26652 
