272e1889631405ec6449dbe7aca42d7d52d48ec7
Michael Sverdlov <sverdlov93@gmail.com>
2022-05-19 09:14:59 +0300

    Update dependency.go

--- commands/testdata/createfixpullrequests/go-proj/dependency.go
+++ commands/testdata/createfixpullrequests/go-proj/dependency.go
@@ -2,11 +2,11 @@ package dependency
 
 import (
 	"fmt"
-	"golang.org/x/crypto/ssh" //#nosec
-	"rsc.io/quote"            //#nosec
+	"golang.org/x/crypto/ssh" // #nosec
+	"rsc.io/quote"            // #nosec
 )
 
 func PrintHello(ssha ssh.AuthMethod) {
-	fmt.Println(quote.Hello())
+	fmt.Println(quote.Hello()) // #nosec
 	return
 }
