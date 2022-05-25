e2deb34e57175245bb32a7c97ec738939eae0ebb
Michael Sverdlov <sverdlov93@gmail.com>
2022-05-25 17:50:01 +0300

    Add missing pull request event name (#82)

--- action/src/main.ts
+++ action/src/main.ts
@@ -8,11 +8,14 @@ async function main() {
         await Utils.addToPath();
         switch (eventName) {
             case "pull_request":
+            case "pull_request_target":
                 await Utils.execScanPullRequest();
                 break;
             case "push":
                 await Utils.execCreateFixPullRequests();
                 break;
+            default:
+                core.setFailed(eventName + " event is not supported by Frogbot");
         }
     } catch (error) {
         core.setFailed((<any>error).message);
