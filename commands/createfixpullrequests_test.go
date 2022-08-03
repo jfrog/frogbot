59998bf1bbe0d715f719e23dfbd389b858c5362b
omerzi <omer3914@gmail.com>
2022-08-03 11:18:36 +0300

    Added test for package fixing stage for each technology

--- commands/createfixpullrequests_test.go
+++ commands/createfixpullrequests_test.go
@@ -1,12 +1,80 @@
 package commands
 
 import (
+	testdatautils "github.com/jfrog/build-info-go/build/testdata"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
-func TestCreateFixPullRequests(t *testing.T) {
-	testCreateFixPullRequests(t, "", "go-proj")
+type FixMavenPackagesTestFunc func(test packageFixTest) error
+type FixGenericPackagesTestFunc func(test packageFixTest) error
+type FixPipPackagesTestFunc func(test packageFixTest) error
+
+type packageFixTest struct {
+	commandArgs       []string
+	technology        string
+	impactedPackaged  string
+	fixVersion        string
+	operator          string
+	packageDescriptor string
+	fixPackageVersion func(test packageFixTest) error
+}
+
+var packageFixTests = []packageFixTest{
+	{commandArgs: []string{"install"}, technology: coreutils.Npm, impactedPackaged: "minimatch", fixVersion: "3.0.2", operator: "@", packageDescriptor: "package.json", fixPackageVersion: getGenericFixPackageVersionFunc()},
+	{commandArgs: []string{"get"}, technology: coreutils.Go, impactedPackaged: "github.com/sassoftware/go-rpmutils", fixVersion: "0.1.1", operator: "@v", packageDescriptor: "go.mod", fixPackageVersion: getGenericFixPackageVersionFunc()},
+	{commandArgs: []string{"up"}, technology: strings.ToLower(coreutils.Yarn), impactedPackaged: "minimist", fixVersion: "1.2.6", operator: "@", packageDescriptor: "package.json", fixPackageVersion: getGenericFixPackageVersionFunc()},
+	{commandArgs: []string{"install"}, technology: coreutils.Pipenv, impactedPackaged: "pyjwt", fixVersion: "2.4.0", operator: "==", packageDescriptor: "Pipfile", fixPackageVersion: getGenericFixPackageVersionFunc()},
+	{technology: coreutils.Maven, impactedPackaged: "junit", fixVersion: "4.11", packageDescriptor: "pom.xml", fixPackageVersion: getMavenFixPackageVersionFunc()},
+	{technology: coreutils.Pip, impactedPackaged: "pyjwt", fixVersion: "2.4.0", packageDescriptor: "requirements.txt", fixPackageVersion: getPipFixPackageVersionFunc()},
+	{technology: coreutils.Pip, impactedPackaged: "pyjwt", fixVersion: "2.4.0", packageDescriptor: "setup.py", fixPackageVersion: getPipFixPackageVersionFunc()},
+}
+
+func getGenericFixPackageVersionFunc() FixGenericPackagesTestFunc {
+	return func(test packageFixTest) error {
+		return fixPackageVersionGeneric(test.commandArgs, test.technology, test.impactedPackaged, test.fixVersion, test.operator)
+	}
+}
+
+func getMavenFixPackageVersionFunc() FixMavenPackagesTestFunc {
+	return func(test packageFixTest) error {
+		mavenDepToPropertyMap := map[string][]string{
+			test.impactedPackaged: {"junit:junit", "3.8.1"},
+		}
+		cfp := CreateFixPullRequestsCmd{
+			mavenDepToPropertyMap: mavenDepToPropertyMap,
+		}
+		return fixPackageVersionMaven(&cfp, test.impactedPackaged, test.fixVersion)
+	}
+}
+
+func getPipFixPackageVersionFunc() FixPipPackagesTestFunc {
+	return func(test packageFixTest) error {
+		return fixPackageVersionPip(test.impactedPackaged, test.fixVersion, test.packageDescriptor)
+	}
+}
+
+func Test_fixPackageVersion(t *testing.T) {
+	testdataDir, err := filepath.Abs(filepath.Join("testdata/projects"))
+	assert.NoError(t, err)
+	for _, test := range packageFixTests {
+		// Create temp technology project
+		projectPath := filepath.Join(testdataDir, test.technology)
+		tmpProjectPath, cleanup := testdatautils.CreateTestProject(t, projectPath)
+		defer cleanup()
+		assert.NoError(t, os.Chdir(tmpProjectPath))
+		t.Run(test.technology, func(t *testing.T) {
+			// Fix impacted package for each technology
+			assert.NoError(t, test.fixPackageVersion(test))
+			file, err := os.ReadFile(test.packageDescriptor)
+			assert.NoError(t, err)
+			assert.Contains(t, string(file), test.fixVersion)
+		})
+	}
 }
 
 ///      1.0         --> 1.0 ≤ x
@@ -57,8 +125,4 @@ func TestGenerateFixBranchName(t *testing.T) {
 			assert.Equal(t, test.expectedName, branchName)
 		})
 	}
-}
-
-func testCreateFixPullRequests(t *testing.T, workingDirectory, projectName string) {
-	// todo: add test
 }
