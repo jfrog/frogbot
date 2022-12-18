4818d9a5c6ab025f49b931a73d8f453ee18bd70b
Omer Zidkoni <omerz@jfrog.com>
2022-12-18 12:19:10 +0200

    Adding tests

--- commands/scanandfixrepos_test.go
+++ commands/scanandfixrepos_test.go
@@ -1,1 +1,31 @@
 package commands
+
+import (
+	"github.com/jfrog/frogbot/commands/utils"
+	"github.com/jfrog/froggit-go/vcsclient"
+	"github.com/jfrog/froggit-go/vcsutils"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestScanAndFixRepos(t *testing.T) {
+	params, restoreEnv := verifyEnv(t)
+	defer restoreEnv()
+
+	server := httptest.NewServer(createHttpHandler(t))
+	defer server.Close()
+
+	client, err := vcsclient.NewClientBuilder(vcsutils.GitHub).ApiEndpoint(server.URL).Token("123456").Build()
+	assert.NoError(t, err)
+
+	var configAggregator utils.FrogbotConfigAggregator
+
+}
+
+func createHttpHandler(t *testing.T) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
