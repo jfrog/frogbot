9dcfbc609a388916e5eef0574873f6b0a72d7583
Omer Zidkoni <omerz@jfrog.com>
2022-12-20 17:52:31 +0200

    add test

--- commands/scanandfixrepos_test.go
+++ commands/scanandfixrepos_test.go
@@ -23,7 +23,7 @@ import (
 	"time"
 )
 
-const cmdDirName = "scanandfixreposs"
+const cmdDirName = "scanandfixrepos"
 
 var testScanAndFixReposConfigPath = filepath.Join("testdata", "config", "frogbot-config-scan-and-fix-repos.yml")
 var testRepositories = []string{"pip-repo", "npm-repo", "mvn-repo"}
