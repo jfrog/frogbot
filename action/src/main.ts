f01e43776288c6bf1a67669f38f8afdc70a468b0
Omer Zidkoni <omerz@jfrog.com>
2022-12-27 10:11:33 +0200

    Fix tests

--- action/src/main.ts
+++ action/src/main.ts
@@ -16,11 +16,15 @@ async function main() {
                 await Utils.execCreateFixPullRequests();
                 break;
             case "schedule":
+                // There are multiple commands with the 'schedule' event, therefore we look at the job name to distinguish between them.
                 if (context.job == "scan-pull-requests") {
                     await Utils.execScanPullRequests();
                     break;
+                } else if (context.job == "scan-and-fix-repos") {
+                    await Utils.execScanAndFixRepos();
+                    break;
                 }
-                await Utils.execScanAndFixRepos();
+                core.setFailed("expected scan-pull-requests or scan-and-fix-repos job names, received: " + context.job);
                 break;
             default:
                 core.setFailed(eventName + " event is not supported by Frogbot");
