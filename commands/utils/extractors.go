7a9ea91ba6457cccd3ae47404cbefb3409cfbfa1
Omer Zidkoni <omerz@jfrog.com>
2023-02-20 12:57:56 +0200

    add logs fix extractors test issue

--- commands/utils/extractors.go
+++ commands/utils/extractors.go
@@ -8,8 +8,6 @@ import (
 	"github.com/jfrog/jfrog-cli-core/v2/artifactory/utils"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
-	gradleutils "github.com/jfrog/jfrog-cli-core/v2/utils/gradle"
-	mvnutils "github.com/jfrog/jfrog-cli-core/v2/utils/mvn"
 	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
 	"github.com/jfrog/jfrog-client-go/utils/log"
 )
@@ -32,16 +30,16 @@ func (ed *extractorDetails) DownloadFromPath() string {
 }
 
 // downloadExtractorsFromRemoteIfNeeded downloads build-info-extractors for air-gapped environments, if they're not exist on the remote repository yet.
-func downloadExtractorsFromRemoteIfNeeded(server *config.ServerDetails, remoteRepoName string, forceDownload bool) error {
+func downloadExtractorsFromRemoteIfNeeded(server *config.ServerDetails, remoteRepoName string, extractorsLocalPath string) (err error) {
 	log.Info("Downloading extractors if needed from", remoteRepoName)
-	mavenExtractorLocalPath, err := mvnutils.GetMavenDependencyLocalPath()
-	if err != nil {
-		return err
+	if extractorsLocalPath == "" {
+		extractorsLocalPath, err = config.GetJfrogDependenciesPath()
+		if err != nil {
+			return err
+		}
 	}
-	gradleExtractorLocalPath, err := gradleutils.GetGradleDependencyLocalPath()
-	if err != nil {
-		return err
-	}
+	mavenExtractorLocalPath := filepath.Join(extractorsLocalPath, "maven", build.MavenExtractorDependencyVersion)
+	gradleExtractorLocalPath := filepath.Join(extractorsLocalPath, "gradle", build.GradleExtractorDependencyVersion)
 	extractors := []extractorDetails{
 		{
 			extractorType: coreutils.Maven.ToString(),
@@ -56,21 +54,20 @@ func downloadExtractorsFromRemoteIfNeeded(server *config.ServerDetails, remoteRe
 			remotePath:    fmt.Sprintf(build.GradleExtractorRemotePath, build.GradleExtractorDependencyVersion),
 		},
 	}
-	return downloadExtractors(remoteRepoName, server, forceDownload, extractors...)
+	return downloadExtractors(remoteRepoName, server, extractors...)
 }
 
-func downloadExtractors(remoteRepoName string, server *config.ServerDetails, forceDownload bool, extractors ...extractorDetails) (err error) {
+func downloadExtractors(remoteRepoName string, server *config.ServerDetails, extractors ...extractorDetails) (err error) {
 	for _, extractor := range extractors {
-		if !forceDownload {
-			var alreadyExist bool
-			if alreadyExist, err = fileutils.IsDirExists(extractor.localPath, false); alreadyExist {
-				log.Debug(extractor.extractorType, "extractor already exists, no download necessary")
-				continue
-			}
-			if err != nil {
-				return err
-			}
+		var alreadyExist bool
+		if alreadyExist, err = fileutils.IsDirExists(extractor.localPath, false); alreadyExist {
+			log.Debug(extractor.extractorType, "extractor already exists, no download necessary")
+			continue
 		}
+		if err != nil {
+			return err
+		}
+		log.Info(extractor.extractorType, "extractor downloads to path:", extractor.localPath)
 		remoteServer := getRemoteServer(server, remoteRepoName)
 		if err = utils.DownloadExtractor(remoteServer, extractor.DownloadFromPath(), extractor.DownloadToPath()); err != nil {
 			return err
