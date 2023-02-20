8300dc8be77e25453b8ec1a3f69b58e9a5e0e88f
Omer Zidkoni <omerz@jfrog.com>
2023-02-20 11:19:21 +0200

    add logs

--- commands/createfixpullrequests_test.go
+++ commands/createfixpullrequests_test.go
@@ -114,9 +114,6 @@ func getMavenFixPackageVersionFunc() func(test packageFixTest) CreateFixPullRequ
 }
 
 func TestFixPackageVersion(t *testing.T) {
-	exists, err := fileutils.IsFileExists("/home/runner/.jfrog/dependencies/maven/2.39.3/build-info-extractor-maven3-2.39.3-uber.jar", false)
-	assert.NoError(t, err)
-	log.Logger.Info("Build-info-extractor exists:", exists)
 	currentDir, testdataDir := getTestDataDir(t)
 	for _, test := range packageFixTests {
 		// Create temp technology project
@@ -133,6 +130,9 @@ func TestFixPackageVersion(t *testing.T) {
 			assert.NoError(t, err)
 			assert.Contains(t, string(file), test.fixVersion)
 		})
+		exists, err := fileutils.IsFileExists("/home/runner/.jfrog/dependencies/maven/2.39.3/build-info-extractor-maven3-2.39.3-uber.jar", false)
+		assert.NoError(t, err)
+		log.Logger.Info("tech:", test.technology, "Build-info-extractor exists:", exists)
 	}
 	assert.NoError(t, os.Chdir(currentDir))
 }
