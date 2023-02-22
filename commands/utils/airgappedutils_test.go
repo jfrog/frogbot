fea7fe580dbfb454d5b1bc1af53ad9460917e9a5
Omer Zidkoni <omerz@jfrog.com>
2023-02-22 12:07:34 +0200

    fix race condition

--- commands/utils/airgappedutils_test.go
+++ commands/utils/airgappedutils_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/jfrog/jfrog-client-go/auth"
 	"github.com/jfrog/jfrog-client-go/http/jfroghttpclient"
 	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
-	"github.com/jfrog/jfrog-client-go/utils/log"
 	"github.com/stretchr/testify/assert"
 	"path/filepath"
 	"runtime"
@@ -105,17 +104,17 @@ func TestResolveDependencies(t *testing.T) {
 				}},
 			resolveFunc: resolveNpmDependencies,
 		},
-		//{
-		//	name: "Resolve Yarn dependencies",
-		//	tech: "yarn",
-		//	scanSetup: &ScanSetup{
-		//		ServerDetails: &params,
-		//		Project: Project{
-		//			InstallCommandName: "yarn",
-		//			InstallCommandArgs: []string{"install"},
-		//		}},
-		//	resolveFunc: resolveYarnDependencies,
-		//},
+		{
+			name: "Resolve Yarn dependencies",
+			tech: "yarn",
+			scanSetup: &ScanSetup{
+				ServerDetails: &params,
+				Project: Project{
+					InstallCommandName: "yarn",
+					InstallCommandArgs: []string{"install"},
+				}},
+			resolveFunc: resolveYarnDependencies,
+		},
 		{
 			name: "Resolve .NET dependencies",
 			tech: "dotnet",
@@ -131,14 +130,19 @@ func TestResolveDependencies(t *testing.T) {
 	}
 
 	for _, test := range testCases {
-		// t.Run runs simultaneously, so it is not used, to avoid race condition in the npm and yarn tests while reading and writing the npm.GetConfigList function
-		func(t *testing.T) {
+		func(t *testing.T, scanSetup *ScanSetup) {
 			restoreFunc, repoKey := setTestEnvironment(t, test.tech, &params)
 			defer restoreFunc()
-			test.scanSetup.Project.DepsResolutionRepo = repoKey
-			output, err := test.resolveFunc(test.scanSetup)
+			scanSetup.Project.DepsResolutionRepo = repoKey
+			_, err := test.resolveFunc(scanSetup)
 			assert.NoError(t, err)
-			log.Info(string(output))
-		}(t)
+		}(t, &ScanSetup{
+			ServerDetails: test.scanSetup.ServerDetails,
+			Project: Project{
+				InstallCommandName: test.scanSetup.Project.InstallCommandName,
+				InstallCommandArgs: test.scanSetup.Project.InstallCommandArgs,
+				DepsResolutionRepo: test.scanSetup.Project.DepsResolutionRepo,
+			},
+		})
 	}
 }
