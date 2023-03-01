a79b7fed46c1089bb4b93731ec029def935ba05c
Yahav Itzhak <yahavi@users.noreply.github.com>
2023-03-01 14:13:29 +0200

    Regex patterns are not efficient enough (#242)

--- commands/utils/mavenutils.go
+++ commands/utils/mavenutils.go
@@ -8,51 +8,80 @@ import (
 	"strings"
 )
 
+const (
+	modulePattern       = "<module>(\\S|\\s)*?<\\/module>"
+	dependenciesPattern = "(<dependency>(\\S|\\s)*?<\\/dependency>)|(<plugin>(\\S|\\s)*?<\\/plugin>)"
+)
+
+var (
+	moduleRegexp       = regexp.MustCompile(modulePattern)
+	dependenciesRegexp = regexp.MustCompile(dependenciesPattern)
+)
+
 func GetVersionProperties(projectPath string, depToPropertyMap map[string][]string) error {
 	contentBytes, err := os.ReadFile(filepath.Join(projectPath, "pom.xml")) // #nosec G304
 	if err != nil {
 		return err
 	}
-	dependenciesPattern := "(<dependency>(.|\\s)*?<\\/dependency>)|(<plugin>(.|\\s)*?<\\/plugin>)"
-	dependenciesRegexp, err := regexp.Compile(dependenciesPattern)
+	mavenDependencies, err := getDependenciesFromPomXml(contentBytes)
 	if err != nil {
 		return err
 	}
-	dependencyStrings := dependenciesRegexp.FindAll(contentBytes, -1)
-	for _, depStr := range dependencyStrings {
-		dep := &mavenDependency{}
-		err = xml.Unmarshal(depStr, dep)
-		if err != nil {
-			return err
-		}
-		depName := dep.GroupId + ":" + dep.ArtifactId
+	for _, mavenDependency := range mavenDependencies {
+		depName := mavenDependency.GroupId + ":" + mavenDependency.ArtifactId
 		if _, exist := depToPropertyMap[depName]; !exist {
 			depToPropertyMap[depName] = []string{}
 		}
-		if strings.HasPrefix(dep.Version, "${") {
-			depToPropertyMap[depName] = append(depToPropertyMap[dep.GroupId+":"+dep.ArtifactId], strings.TrimPrefix(strings.TrimSuffix(dep.Version, "}"), "${"))
+		if strings.HasPrefix(mavenDependency.Version, "${") {
+			depToPropertyMap[depName] = append(depToPropertyMap[mavenDependency.GroupId+":"+mavenDependency.ArtifactId], strings.TrimPrefix(strings.TrimSuffix(mavenDependency.Version, "}"), "${"))
 		}
 	}
 
-	modulePattern := "<module>(.|\\s)*?<\\/module>"
-	moduleRegexp, err := regexp.Compile(modulePattern)
-	if err != nil {
-		return err
-	}
-	moduleStrings := moduleRegexp.FindAllString(string(contentBytes), -1)
-	for _, moduleStr := range moduleStrings {
-		modulePath := strings.TrimPrefix(strings.TrimSuffix(moduleStr, "</module>"), "<module>")
-		modulePath = strings.TrimSpace(modulePath)
-		err = GetVersionProperties(filepath.Join(projectPath, modulePath), depToPropertyMap)
-		if err != nil {
+	for _, moduleStr := range getMavenModuleFromPomXml(contentBytes) {
+		if err = GetVersionProperties(filepath.Join(projectPath, moduleStr), depToPropertyMap); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
+// Extract all dependencies from the input pom.xml
+// pomXmlContent - The pom.xml content
+func getDependenciesFromPomXml(pomXmlContent []byte) ([]mavenDependency, error) {
+	var results []mavenDependency
+	dependencyStrings := dependenciesRegexp.FindAll(pomXmlContent, -1)
+	for _, depStr := range dependencyStrings {
+		dep := &mavenDependency{}
+		err := xml.Unmarshal(depStr, dep)
+		if err != nil {
+			return []mavenDependency{}, err
+		}
+		dep.trimSpaces()
+		results = append(results, *dep)
+	}
+	return results, nil
+}
+
+// Extract all modules from pom.xml
+// pomXmlContent - The pom.xml content
+func getMavenModuleFromPomXml(pomXmlContent []byte) []string {
+	var results []string
+	moduleStrings := moduleRegexp.FindAllString(string(pomXmlContent), -1)
+	for _, moduleStr := range moduleStrings {
+		modulePath := strings.TrimPrefix(strings.TrimSuffix(moduleStr, "</module>"), "<module>")
+		results = append(results, strings.TrimSpace(modulePath))
+	}
+	return results
+}
+
 type mavenDependency struct {
 	GroupId    string `xml:"groupId"`
 	ArtifactId string `xml:"artifactId"`
 	Version    string `xml:"version"`
+}
+
+func (md *mavenDependency) trimSpaces() {
+	md.GroupId = strings.TrimSpace(md.GroupId)
+	md.ArtifactId = strings.TrimSpace(md.ArtifactId)
+	md.Version = strings.TrimSpace(md.Version)
 }
