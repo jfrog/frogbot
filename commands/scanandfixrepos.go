1706c9647106c35900ce66a5ae4ea1af4a5adb28
delarea <eyalde@jfrog.com>
2023-06-04 16:45:56 +0300

    remove build status marking

--- commands/scanandfixrepos.go
+++ commands/scanandfixrepos.go
@@ -1,12 +1,9 @@
 package commands
 
 import (
-	"context"
 	"errors"
-	"fmt"
 	"github.com/jfrog/frogbot/commands/utils"
 	"github.com/jfrog/froggit-go/vcsclient"
-	"github.com/jfrog/jfrog-client-go/utils/log"
 	"path/filepath"
 	"strings"
 	"time"
@@ -22,9 +19,7 @@ type ScanAndFixRepositories struct {
 func (saf *ScanAndFixRepositories) Run(configAggregator utils.FrogbotConfigAggregator, client vcsclient.VcsClient) error {
 	var errList strings.Builder
 	for repoNum := range configAggregator {
-		if err := saf.scanAndFixSingleRepository(&configAggregator[repoNum], client); err != nil {
-			errList.WriteString(fmt.Sprintf("repository %s returned the following error: \n%s\n", configAggregator[repoNum].RepoName, err.Error()))
-		}
+		saf.scanAndFixSingleRepository(&configAggregator[repoNum], client, errList)
 	}
 	if errList.String() != "" {
 		return errors.New(errList.String())
@@ -32,33 +27,14 @@ func (saf *ScanAndFixRepositories) Run(configAggregator utils.FrogbotConfigAggre
 	return nil
 }
 
-func (saf *ScanAndFixRepositories) scanAndFixSingleRepository(repoConfig *utils.FrogbotRepoConfig, client vcsclient.VcsClient) error {
+func (saf *ScanAndFixRepositories) scanAndFixSingleRepository(repoConfig *utils.FrogbotRepoConfig, client vcsclient.VcsClient, errList strings.Builder) {
 	for _, branch := range repoConfig.Branches {
-		shouldScan, checkedCommit, err := saf.shouldScanLatestCommit(context.Background(), repoConfig, client, branch)
-		if err != nil {
-			return err
-		}
-		if !shouldScan {
-			log.Info(fmt.Sprintf("Commit '%s' in repo '%s', branch '%s' has already been scanned. Skipping the scan.", checkedCommit, repoConfig.RepoName, branch))
-			continue
-		}
-		if err = saf.downloadAndRunScanAndFix(repoConfig, branch, client); err != nil {
+		if err := saf.downloadAndRunScanAndFix(repoConfig, branch, client); err != nil {
 			if _, isCustomError := err.(*utils.ErrUnsupportedFix); isCustomError {
-				log.Debug(err.Error())
-			} else {
-				// Scan failed, mark commit status failed with error info
-				if e := saf.setCommitBuildStatus(client, repoConfig, vcsclient.Fail, checkedCommit, fmt.Sprintf("Frogbot error: %s", err)); e != nil {
-					return errors.Join(e, err)
-				}
-				return err
+				errList.WriteString(err.Error())
 			}
 		}
-		// Mark successful run
-		if err = saf.setCommitBuildStatus(client, repoConfig, vcsclient.Pass, checkedCommit, utils.CommitStatusDescription); err != nil {
-			return err
-		}
 	}
-	return nil
 }
 
 func (saf *ScanAndFixRepositories) downloadAndRunScanAndFix(repository *utils.FrogbotRepoConfig, branch string, client vcsclient.VcsClient) (err error) {
@@ -86,31 +62,6 @@ func (saf *ScanAndFixRepositories) downloadAndRunScanAndFix(repository *utils.Fr
 
 	cfp := CreateFixPullRequestsCmd{dryRun: saf.dryRun, dryRunRepoPath: filepath.Join(saf.dryRunRepoPath, repository.RepoName)}
 	return cfp.scanAndFixRepository(repository, branch, client)
-}
-
-func (saf *ScanAndFixRepositories) setCommitBuildStatus(client vcsclient.VcsClient, repoConfig *utils.FrogbotRepoConfig, state vcsclient.CommitStatus, commitHash, description string) error {
-	if err := client.SetCommitStatus(context.Background(), state, repoConfig.RepoOwner, repoConfig.RepoName, commitHash, utils.FrogbotCreatorName, description, utils.CommitStatusDetailsUrl); err != nil {
-		return fmt.Errorf("failed to mark last commit build status due to: %s", err.Error())
-	}
-	log.Info(fmt.Sprintf("Commit '%s' in repo '%s', has successfuly marked as %s", commitHash, repoConfig.RepoName, utils.CommitStatusToString(state)))
-	return nil
-}
-
-// Returns true if the latest commit hasn't been scanned
-// or the time passed from the last scan exceeded the configured value.
-func (saf *ScanAndFixRepositories) shouldScanLatestCommit(ctx context.Context, repoConfig *utils.FrogbotRepoConfig, client vcsclient.VcsClient, branch string) (shouldScan bool, commitHash string, err error) {
-	owner := repoConfig.RepoOwner
-	repo := repoConfig.RepoName
-	latestCommit, err := client.GetLatestCommit(ctx, owner, repo, branch)
-	if err != nil {
-		return false, "", err
-	}
-	ref := latestCommit.Hash
-	statuses, err := client.GetCommitStatuses(ctx, owner, repo, ref)
-	if err != nil {
-		return false, "", err
-	}
-	return shouldScanCommitByStatus(statuses), latestCommit.Hash, err
 }
 
 // Returns true if the latest commit status by Frogbot is not successful
