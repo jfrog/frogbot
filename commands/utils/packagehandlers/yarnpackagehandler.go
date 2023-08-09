1596d26dd689b9cf1a6a8417c54d5f91afe1680f
Eran Turgeman <erant@jfrog.com>
2023-08-09 16:07:59 +0300

    updated yarn handler to handle the un-needed node_modules created upon upgrading packages

--- commands/utils/packagehandlers/yarnpackagehandler.go
+++ commands/utils/packagehandlers/yarnpackagehandler.go
@@ -1,17 +1,24 @@
 package packagehandlers
 
 import (
+	"bytes"
+	"errors"
 	biUtils "github.com/jfrog/build-info-go/build/utils"
 	"github.com/jfrog/frogbot/commands/utils"
 	"github.com/jfrog/gofrog/version"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
 	"github.com/jfrog/jfrog-client-go/utils/errorutils"
+	"golang.org/x/exp/rand"
+	"os/exec"
+	"strconv"
+	"time"
 )
 
 const (
 	yarnV2Version          = "2.0.0"
 	yarnV1PackageUpdateCmd = "upgrade"
 	yarnV2PackageUpdateCmd = "up"
+	modulesFolderFlag      = "--modules-folder=./"
 )
 
 type YarnPackageHandler struct {
@@ -40,12 +47,44 @@ func (yarn *YarnPackageHandler) updateDirectDependency(vulnDetails *utils.Vulner
 		return
 	}
 	executableYarnVersion, err := biUtils.GetVersion(yarnExecutablePath, clonedDirPath)
-	if version.NewVersion(executableYarnVersion).Compare(yarnV2Version) <= 0 {
-		// Yarn 2 & 3
+	isV2AndAbove := version.NewVersion(executableYarnVersion).Compare(yarnV2Version) <= 0
+	dirToDeleteName := ""
+	if isV2AndAbove {
 		vulnDetails.Technology.SetPackageInstallationCommand(yarnV2PackageUpdateCmd)
 	} else {
-		// Yarn 1
 		vulnDetails.Technology.SetPackageInstallationCommand(yarnV1PackageUpdateCmd)
+		dirToDeleteName = getRandomToDeleteDirName()
+		extraArgs = append(extraArgs, modulesFolderFlag+dirToDeleteName)
 	}
-	return yarn.CommonPackageHandler.UpdateDependency(vulnDetails, extraArgs...)
+	err = yarn.CommonPackageHandler.UpdateDependency(vulnDetails, extraArgs...)
+	if err != nil {
+		return
+	}
+
+	if !isV2AndAbove {
+		return deleteNodeModulesDir(clonedDirPath, dirToDeleteName)
+	}
+	return
+}
+
+// deleteNodeModulesDir deletes a directory that contains node_modules that is automatically created when upgrading a package
+// in yarn 1 ('yarn install' runs automatically when running 'yarn upgrade')
+func deleteNodeModulesDir(clonedDirPath string, toDelete string) (err error) {
+	command := exec.Command("rm", "-rf", clonedDirPath+"/"+toDelete)
+	command.Dir = clonedDirPath
+	errBuffer := bytes.NewBuffer([]byte{})
+	command.Stderr = errBuffer
+	err = command.Run()
+	if err != nil {
+		err = errors.New("removing " + toDelete + " directory after updating some package has failed:\n" + err.Error())
+	}
+	return
+}
+
+// getRandomToDeleteDirName returns a directory name that starts with 'to_delete' following a pseudo random number
+// this directory is used to store node_modules created when upgrading packages in yarn 1 and needs to be deleted before pushed to VC
+func getRandomToDeleteDirName() string {
+	rand.Seed(uint64(time.Now().UnixNano()))
+	randomNumberString := strconv.Itoa(int(rand.Int63n(1e9)))
+	return "to_delete" + randomNumberString
 }
