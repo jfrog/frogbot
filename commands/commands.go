98b6d8d1641e16cc28419fd367c0766ba6a0a3be
Omer Zidkoni <omerz@jfrog.com>
2023-08-09 15:04:51 +0300

    Fix open pull request against the wrong branch (#421)

--- commands/commands.go
+++ commands/commands.go
@@ -17,10 +17,10 @@ type FrogbotCommand interface {
 	Run(config utils.RepoAggregator, client vcsclient.VcsClient) error
 }
 
-func Exec(command FrogbotCommand, name string) (err error) {
+func Exec(command FrogbotCommand, commandName string) (err error) {
 	// Get frogbotUtils that contains the config, server, and VCS client
 	log.Info("Frogbot version:", utils.FrogbotVersion)
-	frogbotUtils, err := utils.GetFrogbotDetails()
+	frogbotUtils, err := utils.GetFrogbotDetails(commandName)
 	if err != nil {
 		return err
 	}
@@ -47,17 +47,17 @@ func Exec(command FrogbotCommand, name string) (err error) {
 
 	// Send a usage report
 	usageReportSent := make(chan error)
-	go utils.ReportUsage(name, frogbotUtils.ServerDetails, usageReportSent)
+	go utils.ReportUsage(commandName, frogbotUtils.ServerDetails, usageReportSent)
 
 	// Invoke the command interface
-	log.Info(fmt.Sprintf("Running Frogbot %q command", name))
-	err = command.Run(frogbotUtils.Repositories, frogbotUtils.Client)
+	log.Info(fmt.Sprintf("Running Frogbot %q command", commandName))
+	err = command.Run(frogbotUtils.Repositories, frogbotUtils.GitClient)
 
 	// Wait for a signal, letting us know that the usage reporting is done.
 	<-usageReportSent
 
 	if err == nil {
-		log.Info(fmt.Sprintf("Frogbot %q command finished successfully", name))
+		log.Info(fmt.Sprintf("Frogbot %q command finished successfully", commandName))
 	}
 	return err
 }
@@ -65,7 +65,7 @@ func Exec(command FrogbotCommand, name string) (err error) {
 func GetCommands() []*clitool.Command {
 	return []*clitool.Command{
 		{
-			Name:    "scan-pull-request",
+			Name:    utils.ScanPullRequest,
 			Aliases: []string{"spr"},
 			Usage:   "Scans a pull request with JFrog Xray for security vulnerabilities.",
 			Action: func(ctx *clitool.Context) error {
@@ -74,7 +74,7 @@ func GetCommands() []*clitool.Command {
 			Flags: []clitool.Flag{},
 		},
 		{
-			Name:    "create-fix-pull-requests",
+			Name:    utils.CreateFixPullRequests,
 			Aliases: []string{"cfpr"},
 			Usage:   "Scan the current branch and create pull requests with fixes if needed",
 			Action: func(ctx *clitool.Context) error {
@@ -83,7 +83,7 @@ func GetCommands() []*clitool.Command {
 			Flags: []clitool.Flag{},
 		},
 		{
-			Name:    "scan-pull-requests",
+			Name:    utils.ScanPullRequests,
 			Aliases: []string{"sprs"},
 			Usage:   "Scans all the open pull requests within a single or multiple repositories with JFrog Xray for security vulnerabilities",
 			Action: func(ctx *clitool.Context) error {
@@ -92,7 +92,7 @@ func GetCommands() []*clitool.Command {
 			Flags: []clitool.Flag{},
 		},
 		{
-			Name:    "scan-and-fix-repos",
+			Name:    utils.ScanAndFixRepos,
 			Aliases: []string{"safr"},
 			Usage:   "Scan single or multiple repositories and create pull requests with fixes if any security vulnerabilities are found",
 			Action: func(ctx *clitool.Context) error {
