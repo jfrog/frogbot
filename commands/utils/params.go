fcebcb89602c90ccee5fd42213b34a0227b957ac
delarea <eyalde@jfrog.com>
2023-08-13 10:53:20 +0300

    Separate env params extraction by context

--- commands/utils/params.go
+++ commands/utils/params.go
@@ -208,14 +208,27 @@ func (g *Git) setDefaultsIfNeeded(gitParamsFromEnv *Git) (err error) {
 		}
 		g.RepoName = gitParamsFromEnv.RepoName
 	}
-	if len(g.Branches) == 0 && len(gitParamsFromEnv.Branches) == 0 {
-		var branch string
-		if branch, err = GetBranchFromDotGit(); err != nil {
-			return
+	if g.EmailAuthor == "" {
+		if g.EmailAuthor = getTrimmedEnv(GitEmailAuthorEnv); g.EmailAuthor == "" {
+			g.EmailAuthor = frogbotAuthorEmail
 		}
-		g.Branches = append(g.Branches, branch)
-	} else if len(g.Branches) == 0 {
-		g.Branches = append(g.Branches, gitParamsFromEnv.Branches...)
+	}
+	// When pull request ID is provided, no need to continue and extract unrelated env params.
+	if g.PullRequestDetails.ID != 0 || gitParamsFromEnv.PullRequestDetails.ID != 0 {
+		return
+	}
+	// Continue to extract ScanRepository related env params
+	noBranchesProvidedViaConfig := len(g.Branches) == 0
+	noBranchesProvidedViaEnv := len(gitParamsFromEnv.Branches) == 0
+	if noBranchesProvidedViaConfig {
+		g.Branches = gitParamsFromEnv.Branches
+		if noBranchesProvidedViaEnv {
+			var branch string
+			if branch, err = GetBranchFromDotGit(); err != nil {
+				return
+			}
+			g.Branches = []string{branch}
+		}
 	}
 	if g.BranchNameTemplate == "" {
 		branchTemplate := getTrimmedEnv(BranchNameTemplateEnv)
@@ -233,11 +246,6 @@ func (g *Git) setDefaultsIfNeeded(gitParamsFromEnv *Git) (err error) {
 	if !g.AggregateFixes {
 		if g.AggregateFixes, err = getBoolEnv(GitAggregateFixesEnv, false); err != nil {
 			return
-		}
-	}
-	if g.EmailAuthor == "" {
-		if g.EmailAuthor = getTrimmedEnv(GitEmailAuthorEnv); g.EmailAuthor == "" {
-			g.EmailAuthor = frogbotAuthorEmail
 		}
 	}
 	return
