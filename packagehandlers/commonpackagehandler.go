3829235e20446519f93a04098ab86d731eb1651d
Eran Turgeman <erant@jfrog.com>
2023-08-30 15:05:41 +0300

    minor fix

--- packagehandlers/commonpackagehandler.go
+++ packagehandlers/commonpackagehandler.go
@@ -30,7 +30,7 @@ func GetCompatiblePackageHandler(vulnDetails *utils.VulnerabilityDetails, detail
 		handler = &PythonPackageHandler{pipRequirementsFile: details.PipRequirementsFile}
 	case coreutils.Maven:
 		handler = &MavenPackageHandler{depsRepo: details.DepsRepo, ServerDetails: details.ServerDetails}
-	case coreutils.Nuget, coreutils.Dotnet:
+	case coreutils.Nuget:
 		handler = &NugetPackageHandler{}
 	default:
 		handler = &UnsupportedPackageHandler{}
