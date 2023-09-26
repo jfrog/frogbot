d248d36494e4020f40b3994c7868622bb4a7f788
fsadfsdf <michaelsv@jfrog.com>
2023-09-26 12:01:45 +0300

    Update git action
    Signed-off-by: fsadfsdf <michaelsv@jfrog.com>

--- packagehandlers/mavenpackagehandler.go
+++ packagehandlers/mavenpackagehandler.go
@@ -311,6 +311,7 @@ func (mph *MavenPackageHandler) runMvnCommand(goals []string) (readerOutput []by
 		SetDisableDeploy(true).
 		SetOutputWriter(&buf)
 	readerOutput = make([]byte, 0)
+	err = mvnutils.RunMvn(mvnParams)
 	_, _ = io.ReadFull(&buf, readerOutput)
 	if err = mvnutils.RunMvn(mvnParams); err != nil {
 		if len(readerOutput) > 0 {
