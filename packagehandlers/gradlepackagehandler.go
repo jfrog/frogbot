171e19385eacaa9814541f103e713f0b6c9f1152
Eran Turgeman <erant@jfrog.com>
2023-09-26 18:12:33 +0300

    before deleting old fix flow with precompiled regexps

--- packagehandlers/gradlepackagehandler.go
+++ packagehandlers/gradlepackagehandler.go
@@ -3,7 +3,6 @@ package packagehandlers
 import (
 	"fmt"
 	"github.com/jfrog/frogbot/utils"
-	"github.com/jfrog/gofrog/datastructures"
 	"github.com/jfrog/jfrog-client-go/utils/errorutils"
 	"io/fs"
 	"os"
@@ -17,22 +16,32 @@ const (
 	kotlinBuildFileSuffix         = "build.gradle.kts"
 	apostrophes                   = "[\\\"|\\']"
 	directMapRegexpEntry          = "\\s*%s\\s*[:|=]\\s*"
-	directStringWithVersionRegexp = "%s:%s:%s"
+	directStringWithVersionFormat = "%s:%s:%s"
 )
 
 var regexpPatterns []string
+var directMapWithVersionRegexp string
 
 func init() {
-	// Example: junit:junit:1.0.0
-	regexpPatterns = append(regexpPatterns, directStringWithVersionRegexp)
+	/*
+		// Example: junit:junit:1.0.0
+		regexpPatterns = append(regexpPatterns, directStringWithVersionFormat)
 
+		groupEntry := getMapRegexpEntry("group")
+		nameEntry := getMapRegexpEntry("name")
+		versionEntry := getMapRegexpEntry("version")
+
+		// Example: group: "junit", name: "junit", version: "1.0.0" | group = "junit", name = "junit", version = "1.0.0"
+		directMapRegexpPattern := groupEntry + "," + nameEntry + "," + versionEntry
+		regexpPatterns = append(regexpPatterns, directMapRegexpPattern)
+	*/
+
+	// Initializing a regexp pattern for map dependencies
+	// Example: group: "junit", name: "junit", version: "1.0.0" | group = "junit", name = "junit", version = "1.0.0"
 	groupEntry := getMapRegexpEntry("group")
 	nameEntry := getMapRegexpEntry("name")
 	versionEntry := getMapRegexpEntry("version")
-
-	// Example: group: "junit", name: "junit", version: "1.0.0" | group = "junit", name = "junit", version = "1.0.0"
-	directMapRegexpPattern := groupEntry + "," + nameEntry + "," + versionEntry
-	regexpPatterns = append(regexpPatterns, directMapRegexpPattern)
+	directMapWithVersionRegexp = groupEntry + "," + nameEntry + "," + versionEntry
 }
 
 type GradlePackageHandler struct {
@@ -109,11 +118,6 @@ func getDescriptorFilesPaths() (descriptorFilesPaths []string, err error) {
 
 // fixVulnerabilityIfExists fixes all direct occurrences (string/map) of the given vulnerability in the given descriptor file if vulnerability occurs
 func fixVulnerabilityIfExists(descriptorFilePath string, vulnDetails *utils.VulnerabilityDetails) (err error) {
-	vulnerabilityPatterns, err := getPatternCompilersForVulnerability(vulnDetails)
-	if err != nil {
-		return
-	}
-
 	byteFileContent, err := os.ReadFile(descriptorFilePath)
 	if err != nil {
 		err = fmt.Errorf("couldn't read file '%s': %s", descriptorFilePath, err.Error())
@@ -121,18 +125,46 @@ func fixVulnerabilityIfExists(descriptorFilePath string, vulnDetails *utils.Vuln
 	}
 	fileContent := string(byteFileContent)
 
-	for _, regexpCompiler := range vulnerabilityPatterns {
-		if matches := regexpCompiler.FindAllString(fileContent, -1); matches != nil {
-			set := datastructures.MakeSet[string]()
-			for _, match := range matches {
-				set.Add(strings.TrimSpace(match))
-			}
-			for _, entry := range set.ToSlice() {
-				modifiedContent := strings.Replace(entry, vulnDetails.ImpactedDependencyVersion, vulnDetails.SuggestedFixedVersion, 1)
-				fileContent = strings.ReplaceAll(fileContent, entry, modifiedContent)
-			}
+	depGroup, depName, err := getVulnerabilityGroupAndName(vulnDetails.ImpactedDependencyName)
+	if err != nil {
+		return
+	}
+	// Fixing all vulnerable rows in string format
+	directStringVulnerableRow := fmt.Sprintf(directStringWithVersionFormat, depGroup, depName, vulnDetails.ImpactedDependencyVersion)
+	directStringFixedRow := fmt.Sprintf(directStringWithVersionFormat, depGroup, depName, vulnDetails.SuggestedFixedVersion)
+	fileContent = strings.ReplaceAll(fileContent, directStringVulnerableRow, directStringFixedRow)
+
+	// Fixing all vulnerable rows in a map format
+	mapRegexpForVulnerability := fmt.Sprintf(directMapWithVersionRegexp, depGroup, depName, vulnDetails.ImpactedDependencyVersion)
+	regexpCompiler := regexp.MustCompile(mapRegexpForVulnerability)
+	if rowsMatches := regexpCompiler.FindAllString(fileContent, -1); rowsMatches != nil {
+		for _, entry := range rowsMatches {
+			fixedRow := strings.Replace(entry, vulnDetails.ImpactedDependencyVersion, vulnDetails.SuggestedFixedVersion, 1)
+			fileContent = strings.ReplaceAll(fileContent, entry, fixedRow)
 		}
 	}
+
+	/*
+		vulnerabilityPatterns, err := getPatternCompilersForVulnerability(vulnDetails)
+		if err != nil {
+			return
+		}
+
+		for _, regexpCompiler := range vulnerabilityPatterns {
+			if matches := regexpCompiler.FindAllString(fileContent, -1); matches != nil {
+				uniqueMatches := datastructures.MakeSet[string]()
+				for _, match := range matches {
+					uniqueMatches.Add(strings.TrimSpace(match))
+				}
+
+				for _, entry := range uniqueMatches.ToSlice() {
+					modifiedContent := strings.Replace(entry, vulnDetails.ImpactedDependencyVersion, vulnDetails.SuggestedFixedVersion, 1)
+					fileContent = strings.ReplaceAll(fileContent, entry, modifiedContent)
+				}
+			}
+		}
+
+	*/
 
 	err = writeUpdatedBuildFile(descriptorFilePath, fileContent)
 	return
