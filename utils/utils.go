4e0c42a3753db3cc11641a2a81d54a7ddf2d4aaf
Eran Turgeman <erant@jfrog.com>
2023-10-05 11:10:33 +0300

    fixing the check for isDirectDependency to iterate over all provided paths and not just the first entry

--- utils/utils.go
+++ utils/utils.go
@@ -314,7 +314,15 @@ func IsDirectDependency(impactPath [][]formats.ComponentRow) (bool, error) {
 	if len(impactPath) == 0 {
 		return false, fmt.Errorf("invalid impact path provided")
 	}
-	return len(impactPath[0]) < 3, nil
+
+	for _, path := range impactPath {
+		// Impacted path always starts with the name of the project's directory and ends with the impacted package.
+		// Therefore if the length of the path is less than 2 it means the dependency is a direct one
+		if len(path) < 3 {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
 func validateBranchName(branchName string) error {
