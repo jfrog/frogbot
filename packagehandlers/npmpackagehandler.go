ca1aebaca6048e6469de80f63f7bc7cd89f608ea
Eran Turgeman <erant@jfrog.com>
2023-10-24 12:59:18 +0300

    fix bug: node_modules are pushed into the fixed PR

--- packagehandlers/npmpackagehandler.go
+++ packagehandlers/npmpackagehandler.go
@@ -1,6 +1,13 @@
 package packagehandlers
 
-import "github.com/jfrog/frogbot/utils"
+import (
+	"errors"
+	"fmt"
+	"github.com/jfrog/frogbot/utils"
+	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
+	"os"
+	"path/filepath"
+)
 
 type NpmPackageHandler struct {
 	CommonPackageHandler
@@ -19,5 +26,39 @@ func (npm *NpmPackageHandler) UpdateDependency(vulnDetails *utils.VulnerabilityD
 }
 
 func (npm *NpmPackageHandler) updateDirectDependency(vulnDetails *utils.VulnerabilityDetails) (err error) {
-	return npm.CommonPackageHandler.UpdateDependency(vulnDetails, vulnDetails.Technology.GetPackageInstallationCommand())
+	tmpDir, err := fileutils.CreateTempDir()
+	if err != nil {
+		err = fmt.Errorf("failed to create temporary dir during '%s' dependency fix: %s", vulnDetails.ImpactedDependencyName, err.Error())
+		return
+	}
+	defer func() {
+		err = errors.Join(err, fileutils.RemoveTempDir(tmpDir))
+	}()
+
+	curWd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+
+	packageJsonFilePath := filepath.Join(curWd, "package.json")
+	updatedPackageJsonFilePath := filepath.Join(tmpDir, "package.json")
+
+	// Package.json is assumed to be in the current working dir
+	// We move package.json to a temp dir in order to execute the fix and leave the original dir unchanged but the updated package.json
+	err = fileutils.MoveFile(packageJsonFilePath, updatedPackageJsonFilePath)
+	if err != nil {
+		err = fmt.Errorf("couldn't move package.json to a temporary dir in order to execute the fix for '%s' dependency: %s", vulnDetails.ImpactedDependencyName, err.Error())
+		return
+	}
+	err = npm.CommonPackageHandler.UpdateDependency(vulnDetails, vulnDetails.Technology.GetPackageInstallationCommand(), "--prefix", tmpDir)
+	if err != nil {
+		err = fmt.Errorf("failed during executing fix command for '%s' dependency: %s", vulnDetails.ImpactedDependencyName, err.Error())
+		return
+	}
+
+	err = fileutils.MoveFile(updatedPackageJsonFilePath, packageJsonFilePath)
+	if err != nil {
+		err = fmt.Errorf("couldn't move package.json from a temporary dir to the original working dir '%s': %s", curWd, err.Error())
+	}
+	return
 }
