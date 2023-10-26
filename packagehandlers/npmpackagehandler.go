b951ad5bb4a230c9b696a5dd9d0cb2b2f57df456
Eran Turgeman <erant@jfrog.com>
2023-10-26 16:14:29 +0300

    fixed issues

--- packagehandlers/npmpackagehandler.go
+++ packagehandlers/npmpackagehandler.go
@@ -1,6 +1,7 @@
 package packagehandlers
 
 import (
+	"fmt"
 	"github.com/jfrog/frogbot/utils"
 	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
 	"path/filepath"
@@ -29,6 +30,10 @@ func (npm *NpmPackageHandler) UpdateDependency(vulnDetails *utils.VulnerabilityD
 
 func (npm *NpmPackageHandler) updateDirectDependency(vulnDetails *utils.VulnerabilityDetails) (err error) {
 	isNodeModulesExists, err := fileutils.IsDirExists(filepath.Join(".", "node_modules"), false)
+	if err != nil {
+		err = fmt.Errorf("failed while serching for node_modules in project: %s", err.Error())
+		return
+	}
 	if !isNodeModulesExists {
 		// In case node_modules don't exist in current dir the fix will update only package.json and package-lock.json
 		return npm.CommonPackageHandler.UpdateDependency(vulnDetails, vulnDetails.Technology.GetPackageInstallationCommand(), npmInstallPackageLockOnlyFlag, npmInstallIgnoreScriptsFlag)
