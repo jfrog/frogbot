a83019d7db3c7293ec9a47ef1da7503e529dbad3
Eran Turgeman <erant@jfrog.com>
2023-11-16 16:26:07 +0200

    .

--- scanrepository/scanrepository.go
+++ scanrepository/scanrepository.go
@@ -309,6 +309,7 @@ func (cfp *ScanRepositoryCmd) fixSinglePackageAndCreatePR(vulnDetails *utils.Vul
 			err = fmt.Errorf("failed while creating branch %s: %s", fixBranchName, err.Error())
 			return
 		}
+
 	} else {
 		if err = cfp.gitManager.CreateBranchAndCheckout(fixBranchName); err != nil {
 			err = fmt.Errorf("failed while creating branch %s: %s", fixBranchName, err.Error())
