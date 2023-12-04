cd0159fbe85295d394bc7a9aadb1bc6965544702
Omer Zidkoni <omerz@jfrog.com>
2023-12-04 13:48:36 +0200

    Semantics

--- gitlab_test.go
+++ gitlab_test.go
@@ -13,26 +13,26 @@ const (
 	gitlabGitCloneUrl         = "https://gitlab.com/frogbot-test2/integration.git"
 )
 
-func buildGitlabReposClient(t *testing.T, gitlabToken string) vcsclient.VcsClient {
+func buildGitLabClient(t *testing.T, gitlabToken string) vcsclient.VcsClient {
 	azureClient, err := vcsclient.NewClientBuilder(vcsutils.GitLab).Token(gitlabToken).Build()
 	assert.NoError(t, err)
 	return azureClient
 }
 
-func buildGitlabReposIntegrationTestDetails(t *testing.T) *IntegrationTestDetails {
+func buildGitLabIntegrationTestDetails(t *testing.T) *IntegrationTestDetails {
 	integrationRepoToken := getIntegrationToken(t, gitlabIntegrationTokenEnv)
 	testDetails := NewIntegrationTestDetails(integrationRepoToken, string(utils.GitLab), gitlabGitCloneUrl, "frogbot-test2")
 	return testDetails
 }
 
 func TestGitLab_ScanPullRequestIntegration(t *testing.T) {
-	testDetails := buildGitlabReposIntegrationTestDetails(t)
-	gitlabClient := buildGitlabReposClient(t, testDetails.GitToken)
+	testDetails := buildGitLabIntegrationTestDetails(t)
+	gitlabClient := buildGitLabClient(t, testDetails.GitToken)
 	runScanPullRequestCmd(t, gitlabClient, testDetails)
 }
 
-func TestGitLabRepos_ScanRepositoryIntegration(t *testing.T) {
-	testDetails := buildGitlabReposIntegrationTestDetails(t)
-	gitlabClient := buildGitlabReposClient(t, testDetails.GitToken)
+func TestGitLab_ScanRepositoryIntegration(t *testing.T) {
+	testDetails := buildGitLabIntegrationTestDetails(t)
+	gitlabClient := buildGitLabClient(t, testDetails.GitToken)
 	runScanRepositoryCmd(t, gitlabClient, testDetails)
 }
