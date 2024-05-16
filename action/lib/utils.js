7eadb9d0cb4f6630a80abf4d3f33db715ecb26e9
carmith <carmith@jfrog.com>
2024-05-16 09:50:34 +0300

    tests - trying to trigger run

--- action/lib/utils.js
+++ action/lib/utils.js
@@ -204,6 +204,7 @@ class Utils {
     static getAndValidatePlatformUrl() {
         var _a;
         return __awaiter(this, void 0, void 0, function* () {
+            // First Verify JF_URL is provided through environment variable
             let jfrogUrl = (_a = process.env.JF_URL) !== null && _a !== void 0 ? _a : '';
             if (!jfrogUrl) {
                 throw new Error('JF_URL must be provided and point on your full platform URL, for example: https://mycompany.jfrog.io/');
