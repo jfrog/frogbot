5a937970e4b4ba353ba4eede65cdcd8a814b9fca
Yehuda Rosenberg <113116347+yehudar-jfrog@users.noreply.github.com>
2024-08-14 12:15:01 +0300

    Delete action/node_modules directory

--- action/node_modules/simple-git/dist/src/lib/plugins/spawn-options-plugin.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { SpawnOptions } from 'child_process';
-import { SimpleGitPlugin } from './simple-git-plugin';
-export declare function spawnOptionsPlugin(spawnOptions: Partial<SpawnOptions>): SimpleGitPlugin<'spawn.options'>;
