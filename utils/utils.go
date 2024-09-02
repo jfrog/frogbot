c2d2a8650ec61d50caa02e792a675e9ef5d4b511
attiasas <assafattias93@gmail.com>
2024-09-02 17:33:47 +0300

    Fix breaking changes after security PR

--- utils/utils.go
+++ utils/utils.go
@@ -13,7 +13,6 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/jfrog/frogbot/v2/utils/outputwriter"
 	"github.com/jfrog/froggit-go/vcsclient"
 	"github.com/jfrog/gofrog/version"
 	"github.com/jfrog/jfrog-cli-core/v2/common/commands"
@@ -27,7 +26,6 @@ import (
 	"github.com/jfrog/jfrog-client-go/utils/errorutils"
 	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
 	"github.com/jfrog/jfrog-client-go/utils/log"
-	"github.com/owenrumney/go-sarif/v2/sarif"
 )
 
 const (
@@ -238,56 +236,12 @@ func UploadSarifResultsToGithubSecurityTab(scanResults *xrayutils.Results, repo 
 	return nil
 }
 
-func prepareRunsForGithubReport(runs []*sarif.Run) []*sarif.Run {
-	for _, run := range runs {
-		for _, rule := range run.Tool.Driver.Rules {
-			// Github security tab can display markdown content on Help attribute and not description
-			if rule.Help == nil && rule.FullDescription != nil {
-				rule.Help = rule.FullDescription
-			}
-		}
-		// Github security tab can't accept results without locations, remove them
-		results := []*sarif.Result{}
-		for _, result := range run.Results {
-			if len(result.Locations) == 0 {
-				continue
-			}
-			results = append(results, result)
-		}
-		run.Results = results
-	}
-	convertToRelativePath(runs)
-	// If we upload to Github security tab multiple runs, it will only display the last run as active issues.
-	// Combine all runs into one run with multiple invocations, so the Github security tab will display all the results as not resolved.
-	combined := sarif.NewRunWithInformationURI(sarifToolName, outputwriter.FrogbotRepoUrl)
-	sarifutils.AggregateMultipleRunsIntoSingle(runs, combined)
-	return []*sarif.Run{combined}
-}
-
-func convertToRelativePath(runs []*sarif.Run) {
-	for _, run := range runs {
-		for _, result := range run.Results {
-			for _, location := range result.Locations {
-				sarifutils.SetLocationFileName(location, sarifutils.GetRelativeLocationFileName(location, run.Invocations))
-			}
-			for _, flows := range result.CodeFlows {
-				for _, flow := range flows.ThreadFlows {
-					for _, location := range flow.Locations {
-						sarifutils.SetLocationFileName(location.Location, sarifutils.GetRelativeLocationFileName(location.Location, run.Invocations))
-					}
-				}
-			}
-		}
-	}
-}
-
 func GenerateFrogbotSarifReport(extendedResults *xrayutils.Results, isMultipleRoots bool, allowedLicenses []string) (string, error) {
-	sarifReport, err := xrayutils.GenereateSarifReportFromResults(extendedResults, isMultipleRoots, false, allowedLicenses)
+	sarifReport, err := xrayutils.GenerateSarifReportFromResults(extendedResults, isMultipleRoots, false, allowedLicenses)
 	if err != nil {
 		return "", err
 	}
-	sarifReport.Runs = prepareRunsForGithubReport(sarifReport.Runs)
-	return sarifutils.ConvertSarifReportToString(sarifReport)
+	return xrayutils.WriteSarifResultsAsString(sarifReport, false)
 }
 
 func DownloadRepoToTempDir(client vcsclient.VcsClient, repoOwner, repoName, branch string) (wd string, cleanup func() error, err error) {
