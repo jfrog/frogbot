1ee9e114fb718019a6fb98c46ac6ba2ca3868bb4
Eran Turgeman <erant@jfrog.com>
2024-10-15 14:56:56 +0300

    added new env var

--- utils/consts.go
+++ utils/consts.go
@@ -34,11 +34,12 @@ const (
 	JfrogConfigProfileEnv  = "JF_CONFIG_PROFILE"
 
 	// Git environment variables
-	GitProvider     = "JF_GIT_PROVIDER"
-	GitRepoOwnerEnv = "JF_GIT_OWNER"
-	GitRepoEnv      = "JF_GIT_REPO"
-	GitProjectEnv   = "JF_GIT_PROJECT"
-	GitUsernameEnv  = "JF_GIT_USERNAME"
+	GitProvider                     = "JF_GIT_PROVIDER"
+	GitRepoOwnerEnv                 = "JF_GIT_OWNER"
+	GitRepoEnv                      = "JF_GIT_REPO"
+	GitProjectEnv                   = "JF_GIT_PROJECT"
+	GitUsernameEnv                  = "JF_GIT_USERNAME"
+	GitUseCurrentLocalRepositoryEnv = "JF_USE_CURRENT_LOCAL_REPOSITORY"
 
 	// Git naming template environment variables
 	BranchNameTemplateEnv       = "JF_BRANCH_NAME_TEMPLATE"
