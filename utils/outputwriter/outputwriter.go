6d01e1f88f8baab165ef984fa94e3d46a9e86f79
Assaf Attias <49212512+attiasas@users.noreply.github.com>
2025-11-24 23:05:58 +0200

    Update documentation Url (#956)

--- utils/outputwriter/outputwriter.go
+++ utils/outputwriter/outputwriter.go
@@ -10,6 +10,83 @@ import (
 	"github.com/jfrog/jfrog-client-go/utils/log"
 )
 
+const (
+	SecretsEmailCSS = `body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+        }
+        table {
+            border-collapse: collapse;
+            width: 80%;
+        }
+        th, td {
+            padding: 10px;
+            border: 1px solid #ccc;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+        .table-container {
+            max-width: 700px;
+            padding: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+            overflow: hidden;
+            background-color: #fff;
+			margin-top: 10px;
+        }
+        .ignore-comments {
+            margin-top: 10px;
+			margin-bottom: 5px;
+            border-radius: 5px;
+        }`
+	//#nosec G101 -- full secrets would not be hard coded
+	SecretsEmailHTMLTemplate = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Frogbot Secret Detection</title>
+    <style>
+        %s
+    </style>
+</head>
+<body>
+	<div>
+		The following potential exposed secrets in your <a href="%s">%s</a> have been detected by <a href="https://jfrog.com/help/r/jfrog-security-user-guide/shift-left-on-security/frogbot">Frogbot</a>
+		<br/>
+		<table class="table-container">
+            <thead>
+                <tr>
+                    <th>FILE</th>
+                    <th>LINE:COLUMN</th>
+                    <th>SECRET</th>
+                </tr>
+            </thead>
+            <tbody>
+                %s
+            </tbody>
+        </table>
+		<div class="ignore-comments">
+		<b>NOTE:</b> If you'd like Frogbot to ignore the lines with the potential secrets, add a comment that includes the <b>jfrog-ignore</b> keyword above the lines with the secrets.	
+		</div>
+	</div>
+</body>
+</html>`
+	//#nosec G101 -- full secrets would not be hard coded
+	SecretsEmailTableRow = `
+				<tr>
+					<td> %s </td>
+					<td> %d:%d </td>
+					<td> %s </td>
+				</tr>`
+)
+
 // The OutputWriter interface allows Frogbot output to be written in an appropriate way for each git provider.
 // Some git providers support markdown only partially, whereas others support it fully.
 type OutputWriter interface {
