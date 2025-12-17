eaf2e8053b089eaddd1d942bda699ab3f2f9bdd0
Eran Turgeman <erant@jfrog.com>
2025-12-17 15:46:53 +0200

    improve config profile tests

--- utils/testsutils.go
+++ utils/testsutils.go
@@ -234,6 +234,58 @@ func CreateXscMockServerForConfigProfile(t *testing.T, xrayVersion string) (mock
 	return
 }
 
+func getComparisonConfigProfile() *services.ConfigProfile {
+	return &services.ConfigProfile{
+		ProfileName: "default-profile",
+		GeneralConfig: services.GeneralConfig{
+			ScannersDownloadPath:    "https://repo.example.com/releases",
+			GeneralExcludePatterns:  []string{"*.log*", "*.tmp*"},
+			FailUponAnyScannerError: true,
+		},
+		FrogbotConfig: services.FrogbotConfig{
+			AggregateFixes:                      true,
+			HideSuccessBannerForNoIssues:        false,
+			BranchNameTemplate:                  "frogbot-${IMPACTED_PACKAGE}-${BRANCH_NAME_HASH}",
+			PrTitleTemplate:                     "[🐸 Frogbot] Upgrade {IMPACTED_PACKAGE} to {FIX_VERSION}",
+			CommitMessageTemplate:               "Upgrade {IMPACTED_PACKAGE} to {FIX_VERSION}",
+			ShowSecretsAsPrComment:              false,
+			CreateAutoFixPr:                     true,
+			IncludeVulnerabilitiesAndViolations: false,
+		},
+		Modules: []services.Module{
+			{
+				ModuleName:   "default-module",
+				PathFromRoot: ".",
+				ScanConfig: services.ScanConfig{
+					ScaScannerConfig: services.ScaScannerConfig{
+						EnableScaScan:   true,
+						ExcludePatterns: []string{"**/build/**"},
+					},
+					ContextualAnalysisScannerConfig: services.CaScannerConfig{
+						EnableCaScan:    true,
+						ExcludePatterns: []string{"**/docs/**"},
+					},
+					SastScannerConfig: services.SastScannerConfig{
+						EnableSastScan:  true,
+						ExcludePatterns: []string{"**/_test.go/**"},
+						ExcludeRules:    []string{"xss-injection"},
+					},
+					SecretsScannerConfig: services.SecretsScannerConfig{
+						EnableSecretsScan:   true,
+						ValidateSecrets:     true,
+						ExcludePatterns:     []string{"**/_test.go/**"},
+						EnableCustomSecrets: true,
+					},
+					IacScannerConfig: services.IacScannerConfig{
+						EnableIacScan:   true,
+						ExcludePatterns: []string{"*.tfstate"},
+					},
+				},
+			},
+		},
+	}
+}
+
 // CreateMockServerForDependencySubmission creates a mock GitHub server for testing dependency submission
 func CreateMockServerForDependencySubmission(t *testing.T, owner, repo string) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
