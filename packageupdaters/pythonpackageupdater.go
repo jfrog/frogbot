5a2dfa69533ba5f9304d9e5b43627accec3848cb
Or Toren <ort@jfrog.com>
2026-02-18 15:21:33 +0200

    pip fix

--- packageupdaters/pythonpackageupdater.go
+++ packageupdaters/pythonpackageupdater.go
@@ -21,7 +21,6 @@ const (
 	PythonPackageRegexSuffix = "\\s*(([\\=\\<\\>\\~]=)|([\\>\\<]))\\s*(\\.|\\d)*(\\d|(\\.\\*))(\\,\\s*(([\\=\\<\\>\\~]=)|([\\>\\<])).*\\s*(\\.|\\d)*(\\d|(\\.\\*)))?"
 )
 
-// PythonPackageUpdater Handles all the python package mangers as they share behavior
 type PythonPackageUpdater struct {
 	pipRequirementsFile string
 	CommonPackageUpdater
@@ -53,25 +52,21 @@ func (py *PythonPackageUpdater) updateDirectDependency(vulnDetails *utils.Vulner
 }
 
 func (py *PythonPackageUpdater) handlePoetry(vulnDetails *utils.VulnerabilityDetails) (err error) {
-	// Install the desired fixed version
 	if err = py.CommonPackageUpdater.UpdateDependency(vulnDetails, vulnDetails.Technology.GetPackageInstallationCommand()); err != nil {
 		return
 	}
-	// Update Poetry lock file as well
 	return runPackageMangerCommand(techutils.Poetry.GetExecCommandName(), techutils.Poetry.String(), []string{"update"})
 }
 
 func (py *PythonPackageUpdater) handlePip(vulnDetails *utils.VulnerabilityDetails) (err error) {
 	var fixedFile string
-	// This function assumes that the version of the dependencies is statically pinned in the requirements file or inside the 'install_requires' array in the setup.py file
 	fixedPackage := vulnDetails.ImpactedDependencyName + "==" + vulnDetails.SuggestedFixedVersion
 	currentFile, err := py.tryGetRequirementFile()
 	if err != nil {
-		return errors.New("failed to read requirements file: " + err.Error())
+		return errors.New("failed to read pip requirements file: " + err.Error())
 	}
-	// Check both original and lowered package name and replace to only one lowered result
-	// This regex will match the impactedPackage with it's pinned version e.py. PyJWT==1.7.1
-	re := regexp.MustCompile(PythonPackageRegexPrefix + "(" + vulnDetails.ImpactedDependencyName + "|" + strings.ToLower(vulnDetails.ImpactedDependencyName) + ")" + PythonPackageRegexSuffix)
+	re := regexp.MustCompile(PythonPackageRegexPrefix + "(" + vulnDetails.ImpactedDependencyName + "|" +
+		strings.ToLower(vulnDetails.ImpactedDependencyName) + ")" + PythonPackageRegexSuffix)
 	if packageToReplace := re.FindString(currentFile); packageToReplace != "" {
 		fixedFile = strings.Replace(currentFile, packageToReplace, strings.ToLower(fixedPackage), 1)
 	}
@@ -79,7 +74,8 @@ func (py *PythonPackageUpdater) handlePip(vulnDetails *utils.VulnerabilityDetail
 		return fmt.Errorf("impacted package %s not found, fix failed", vulnDetails.ImpactedDependencyName)
 	}
 	if err = os.WriteFile(py.pipRequirementsFile, []byte(fixedFile), 0600); err != nil {
-		err = fmt.Errorf("an error occured while writing the fixed version of %s to the requirements file:\n%s", vulnDetails.SuggestedFixedVersion, err.Error())
+		err = fmt.Errorf("an error occured while writing the fixed version of %s to the requirements file:\n%s",
+			vulnDetails.SuggestedFixedVersion, err.Error())
 	}
 	return
 }
