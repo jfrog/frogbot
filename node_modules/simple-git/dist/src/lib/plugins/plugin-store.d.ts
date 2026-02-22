a201bc034ae36eb2438d7677006a5ddde5097c83
Eran Turgeman <erant@jfrog.com>
2026-02-22 11:02:44 +0200

    remove files

--- node_modules/simple-git/dist/src/lib/plugins/plugin-store.d.ts
+++
@@ -1,6 +0,0 @@
-import { SimpleGitPlugin, SimpleGitPluginType, SimpleGitPluginTypes } from './simple-git-plugin';
-export declare class PluginStore {
-    private plugins;
-    add<T extends SimpleGitPluginType>(plugin: void | SimpleGitPlugin<T> | SimpleGitPlugin<T>[]): () => void;
-    exec<T extends SimpleGitPluginType>(type: T, data: SimpleGitPluginTypes[T]['data'], context: SimpleGitPluginTypes[T]['context']): typeof data;
-}
