01198a6992e6eba34ad640fb20b94975ba6b5f77
Or Toren <ort@jfrog.com>
2026-02-24 12:19:24 +0200

    fixing tests

--- packageupdaters/commonpackageupdater_test.go
+++ packageupdaters/commonpackageupdater_test.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/jfrog/build-info-go/tests"
 	biutils "github.com/jfrog/build-info-go/utils"
-	"github.com/jfrog/jfrog-cli-security/sca/bom/buildinfo/technologies/java"
 	"github.com/jfrog/jfrog-cli-security/utils/formats"
 	"github.com/jfrog/jfrog-cli-security/utils/techutils"
 	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
@@ -327,27 +326,6 @@ func TestUpdateDependency(t *testing.T) {
 					}
 				})
 		}
-	}
-}
-
-func TestPipPackageRegex(t *testing.T) {
-	var pipPackagesRegexTests = []pipPackageRegexTest{
-		{"oslo.config", "oslo.config>=1.12.1,<1.13"},
-		{"oslo.utils", "oslo.utils<5.0,>=4.0.0"},
-		{"paramiko", "paramiko==2.7.2"},
-		{"passlib", "passlib<=1.7.4"},
-		{"PassLib", "passlib<=1.7.4"},
-		{"prance", "prance>=0.9.0"},
-		{"prompt-toolkit", "prompt-toolkit~=1.0.15"},
-		{"pyinotify", "pyinotify>0.9.6"},
-		{"pyjwt", "pyjwt>1.7.1"},
-		{"PyJWT", "pyjwt>1.7.1"},
-		{"urllib3", "urllib3 > 1.1.9, < 1.5.*"},
-	}
-	for _, pack := range pipPackagesRegexTests {
-		re := regexp.MustCompile(PythonPackageRegexPrefix + "(" + pack.packageName + "|" + strings.ToLower(pack.packageName) + ")" + PythonPackageRegexSuffix)
-		found := re.FindString(requirementsFile)
-		assert.Equal(t, pack.expectedRequirement, strings.ToLower(found))
 	}
 }
 
