b37c96379602924117d3fd2615583e7b48ece5a0
Or Toren <ort@jfrog.com>
2026-06-14 11:13:48 +0300

    Fix cyclonedx.Component value→pointer in test struct literals

--- scanrepository/scanrepository_test.go
+++ scanrepository/scanrepository_test.go
@@ -567,7 +567,7 @@ func TestCreateVulnerabilitiesMap(t *testing.T) {
 					Sca: []violationutils.CveViolation{
 						{
 							ScaViolation: violationutils.ScaViolation{
-								ImpactedComponent: cyclonedx.Component{
+								ImpactedComponent: &cyclonedx.Component{
 									BOMRef:     "pkg:npm/viol1@1.0.0",
 									PackageURL: "pkg:npm/viol1@1.0.0",
 								},
@@ -580,7 +580,7 @@ func TestCreateVulnerabilitiesMap(t *testing.T) {
 						},
 						{
 							ScaViolation: violationutils.ScaViolation{
-								ImpactedComponent: cyclonedx.Component{
+								ImpactedComponent: &cyclonedx.Component{
 									BOMRef:     "pkg:npm/viol2@2.0.0",
 									PackageURL: "pkg:npm/viol2@2.0.0",
 								},
