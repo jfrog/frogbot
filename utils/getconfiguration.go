5e772163c99865a665563d5a5873baa4834f8a73
attiasas <assafattias93@gmail.com>
2026-07-30 13:29:36 +0300

    format

--- utils/getconfiguration.go
+++ utils/getconfiguration.go
@@ -69,11 +69,11 @@ func (jp *JFrogPlatform) setJfProjectKeyIfExists() (err error) {
 type Git struct {
 	GitProvider vcsutils.VcsProvider
 	vcsclient.VcsInfo
-	RepoOwner                  string
-	RepoName                   string
-	Branches                   []string
-	PullRequestDetails         vcsclient.PullRequestInfo
-	RepositoryCloneUrl         string
+	RepoOwner                    string
+	RepoName                     string
+	Branches                     []string
+	PullRequestDetails           vcsclient.PullRequestInfo
+	RepositoryCloneUrl           string
 	UploadSbomToVcs              *bool
 	UploadPrSecurityResultsToVcs bool
 	GitlabScanResultsOutputDir   string
